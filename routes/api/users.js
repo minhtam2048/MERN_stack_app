d239bac59bd8a1c442002a8c817f0e0712c80197
minhtam2048 <minhtam2048@gmail.com>
2020-04-05 21:42:27 +0700

    Added register method

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,73 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
-// GET    api/users
-// Test   route
-// access Public
-router.get('/', (req, res) => res.send('User route'));
+const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/User");
+
+// @route    POST api/users
+// Test      route
+// access    Public
+router.post(
+  "/",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Please include valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      //if user exists
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      const avatar = gravatar.url(email, {
+        size: "200",
+        rating: "pg",
+        default: "mm"
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send("User reistered");
+      // Return json webtoken
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 // Connect DB
 connectDB();
 
+// Init middleware: get data from request's body
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API running"));
 
 // Define routes
